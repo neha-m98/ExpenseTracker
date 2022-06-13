f3c032c676f33a1096ec1e337d657b2463fed4d1
neha-m98 <66993152+neha-m98@users.noreply.github.com>
2022-06-13 13:11:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import NewExpense from './components/NewExpenses/NewExpense';
 
 const DUMMY_EXPENSES=[
   {id:'e1',title:'Car Insurance', amount:500.78,date:new Date(2021,2,28)},
-  {id:'e2',title:'Home Loan', amount:1200.34,date:new Date(2020,3,15)},
-  {id:'e3',title:'Health Insurance', amount:200.78,date:new Date(2020,9,8)},
+  {id:'e2',title:'Home Loan', amount:1200.34,date:new Date(2022,3,15)},
+  {id:'e3',title:'Health Insurance', amount:200.78,date:new Date(2020,7,8)},
+   {id:'e3',title:'Car Loan', amount:200.78,date:new Date(2022,9,8)},
+   {id:'e3',title:'Medical Insurance', amount:200.78,date:new Date(2022,9,8)},
   {id:'e4',title:'life Insurance', amount:350.67,date:new Date(2019,7,2)},
   {id:'e4',title:'Vacation', amount:700.67,date:new Date(2021,7,2)}
 ]
